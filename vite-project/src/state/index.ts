cba83427872984aa6966df32b3aa0ebcc8411017
sjbworks <sacred.jizo.bosatz@gmail.com>
2023-04-23 23:58:29 +0900

    wip

--- vite-project/src/state/index.ts
+++ vite-project/src/state/index.ts
@@ -1,7 +1,9 @@
-import { createContext } from "react";
+import { createContext, useReducer, Reducer } from "react";
 import { states } from "./states";
 
 export const useGlobalStateContext = (state: any) => createContext(state);
+
+const module = states.snackbar.snackbar;
 
 type NameSpaces = (typeof states)[keyof typeof states]["namespace"];
 
@@ -10,3 +12,11 @@ type ReducerReturnType<NameSpaces> = void;
 // function useGlobalStateReducer(namespace: string[]) {}
 // export function useGlobalStateReducer(namespace: string | string[]) {
 // }
+
+export const useGlobalState = (namespace: NameSpaces) => {
+  const useGlobalStateContext = createContext();
+  const [state, dispatch] = useReducer(
+    states[namespace]["reducer"],
+    states[namespace][namespace]
+  );
+};
