6f11d21284e60d40ad8de579a2445e5848e1041b
sjbworks <sacred.jizo.bosatz@gmail.com>
2023-04-25 23:57:02 +0900

    state type

--- vite-project/src/state/type/index.ts
+++ vite-project/src/state/type/index.ts
@@ -1,4 +1,6 @@
 import { namespaces } from "../states";
+import { snackbar } from "../states/snackbar";
+import { user } from "../states/user";
 
 type GlobalStateArgs = {
   namespace: string | string[];
@@ -8,4 +10,9 @@ type GlobalStateReturnType = {
   state: string;
 };
 
+const module = { snackbar, user };
+
 export type NameSpaces = (typeof namespaces)[number];
+export type State = {
+  [k in (typeof namespaces)[number]]: (typeof module)[k];
+};
