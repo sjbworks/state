26fdcb24e4609521620af78897d59af5eda15e19
sjbworks <sacred.jizo.bosatz@gmail.com>
2023-05-10 23:55:04 +0900

    init setting for redux

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -4,14 +4,21 @@ import { GlobalStateProvider } from "./state";
 import User from "./User";
 import Snackbar from "./Snackbar";
 import UserAndSnackbar from "./UserAndSnackbar";
+import { Provider } from "react-redux";
+import store from "./reduxStates";
 
 function App() {
   return (
-    <GlobalStateProvider>
-      <User />
-      <Snackbar />
-      <UserAndSnackbar />
-    </GlobalStateProvider>
+    <>
+      <GlobalStateProvider>
+        <User />
+        <Snackbar />
+        <UserAndSnackbar />
+      </GlobalStateProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </>
   );
 }
 
